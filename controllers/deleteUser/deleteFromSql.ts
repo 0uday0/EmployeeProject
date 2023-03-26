a43013b8386966025ed94a0f3972bb827f09b942
0uday0 <104912709+0uday0@users.noreply.github.com>
2023-03-26 20:51:30 +0530

    Update deleteFromSql.ts
    Removed Inactive code

--- controllers/deleteUser/deleteFromSql.ts
+++ controllers/deleteUser/deleteFromSql.ts
@@ -1,21 +1,6 @@
 import { OkPacket } from "mysql2"
 import { pool as connection } from "../../db"
-// export async function deActivateFromUsers (userId:number) {
-//     let deactivateUserQuery = `UPDATE users
-//                                SET user_status = 'InActive'
-//                                WHERE user_id = ?
-//                                 `
-//     return new Promise((resolve,reject)=>{
-//       connection.query<OkPacket>(deactivateUserQuery,[userId],(err,result)=>{
-//         if (err) {
-//          return reject(err)
-//         } else {
-//           console.log("the candidate is deactivated from users")
-//           return resolve(result)
-//         }
-//       })
-//     })                             
-//   }
+
 
 export async function deActivateFromUsers (userId:number) {
     let deactivateUserQuery = `UPDATE users
@@ -23,35 +8,10 @@ export async function deActivateFromUsers (userId:number) {
                                WHERE user_id = ?
                                 `
     await connection.query(deactivateUserQuery,[userId])
-    // return new Promise((resolve,reject)=>{
-    //   connection.query<OkPacket>(deactivateUserQuery,[userId],(err,result)=>{
-    //     if (err) {
-    //      return reject(err)
-    //     } else {
-    //       console.log("the candidate is deactivated from users")
-    //       return resolve(result)
-    //     }
-    //   })
-    // })                              
+                            
   }
 
-// export  async function deActivateFromLocationMapping (userid:number) {
-//     let deactivateUserQuery = `UPDATE user_location_mapping
-//                                SET map_status = 'InActive'
-//                                WHERE map_user_id = ?
-//                                 `
-    
-//     return new Promise((resolve,reject)=>{
-//       connection.query<OkPacket>(deactivateUserQuery,[userid],(err,result)=>{
-//         if (err) {
-//          return reject(err)
-//         } else {
-//           console.log("the candidate is deactivated from location mapping")
-//           return resolve(result)
-//         }
-//       })
-//     })
-//   }
+
 
   export  async function deActivateFromLocationMapping (userid:number) {
     let deactivateUserQuery = `UPDATE user_location_mapping
@@ -59,34 +19,10 @@ export async function deActivateFromUsers (userId:number) {
                                WHERE map_user_id = ?
                                 `
     await connection.query(deactivateUserQuery,[userid])
-    // return new Promise((resolve,reject)=>{
-    //   connection.query<OkPacket>(deactivateUserQuery,[userid],(err,result)=>{
-    //     if (err) {
-    //      return reject(err)
-    //     } else {
-    //       console.log("the candidate is deactivated from location mapping")
-    //       return resolve(result)
-    //     }
-    //   })
-    // })
+  
   }
 
-// export  async function deActivateFromDepartmentMapping(userid:number){
-//     let deactivateUserQuery = `UPDATE user_department_mapping
-//                                SET map_status = 'InActive'
-//                                WHERE map_user_id = ?
-//                                 `
-//     return new Promise((resolve,reject)=>{
-//       connection.query<OkPacket>(deactivateUserQuery,[userid],(err,result)=>{
-//         if (err) {
-//          return reject(err)
-//         } else {
-//           console.log("the candidate is deactivated from department mapping")
-//           return resolve(result)
-//         }
-//       })
-//     })
-//   }
+
 
   export  async function deActivateFromDepartmentMapping(userid:number){
     let deactivateUserQuery = `UPDATE user_department_mapping
